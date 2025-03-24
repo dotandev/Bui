473cf542218ff5a02058febefa233bd21ce3af1d
Biokes <106444765+Biokes@users.noreply.github.com>
2025-03-24 13:07:46 +0100

    [homepage]  added react router

--- src/main.tsx
+++ src/main.tsx
@@ -8,19 +8,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
 import { networkConfig } from "./networkConfig.ts";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme appearance="dark">
-      <QueryClientProvider client={queryClient}>
-        <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-          <WalletProvider autoConnect>
-            <App />
-          </WalletProvider>
-        </SuiClientProvider>
-      </QueryClientProvider>
-    </Theme>
+    <BrowserRouter>
+      <Theme appearance="dark">
+        <QueryClientProvider client={queryClient}>
+          <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+            <WalletProvider autoConnect>
+              <App />
+            </WalletProvider>
+          </SuiClientProvider>
+        </QueryClientProvider>
+      </Theme>
+    </BrowserRouter>
   </React.StrictMode>,
 );
